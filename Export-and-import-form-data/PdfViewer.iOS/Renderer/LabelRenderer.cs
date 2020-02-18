37d80f1fc634683d9f3b16553ceaa300102070d5
Ramkumar Ravy <ramkumarr@syncfusion.com>
2020-02-18 21:17:44 +0530

    Added Export and Import Form Data sample

---
+++ Export-and-import-form-data/PdfViewer.iOS/Renderer/LabelRenderer.cs
@@ -0,0 +1,30 @@
+﻿#region Copyright Syncfusion Inc. 2001-2019.
+// Copyright Syncfusion Inc. 2001-2019. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using UIKit;
+using SampleBrowser.SfPdfViewer;
+using SampleBrowser.SfPdfViewer.iOS;
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+namespace SampleBrowser.SfPdfViewer.iOS
+{
+    public class CustomLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Layer.BorderColor = UIColor.Clear.CGColor;
+                Control.TextColor = UIColor.White;
+            }
+        }
+    }
+}
